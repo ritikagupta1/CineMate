741c9fc82b5c3c466ce15e03559b762ea7f306d6
Ritika Gupta <ritikagupta2020@gmail.com>
2024-10-29 12:58:14 +0530

    Initial Commit

---
+++ CineMate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CineMate
+//
+//  Created by Ritika Gupta on 29/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
